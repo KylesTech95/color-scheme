e332026d1cad9409fc088a864fd098740da71ec7
kylestech95 <kylestech95@gmail.com>
2024-04-13 11:47:11 -0400

    fix: comment post

--- public/main.js
+++ public/main.js
@@ -1,20 +1,20 @@
 const spots = document.querySelectorAll('.choice-spot')
 let r,g,b,colour;
-const postfetch = async(api,d) => {
-    const response = await fetch(api,
-    {
-      method:'POST',
-      mode:"cors",
-      cache:"no-cache",
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: { "Content-Type": "application/json"},
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer",
-      body:JSON.stringify(d),
-    })
-    // testing postFetch
-    return response.json()
-}
+// const postfetch = async(api,d) => {
+//     const response = await fetch(api,
+//     {
+//       method:'POST',
+//       mode:"cors",
+//       cache:"no-cache",
+//       credentials: "same-origin", // include, *same-origin, omit
+//       headers: { "Content-Type": "application/json"},
+//       redirect: "follow", // manual, *follow, error
+//       referrerPolicy: "no-referrer",
+//       body:JSON.stringify(d),
+//     })
+//     // testing postFetch
+//     return response.json()
+// }
 
 
 let arr = []
@@ -27,9 +27,9 @@ for(let i=0; i < 1<<12; i++) {
     arr.push(colour)
     
 }
-spots[0].addEventListener('click', e => {
-    postfetch('/colors',{color:[...arr]}).then((data)=>{
-        return data.color
-    })
-})
+// spots[0].addEventListener('click', e => {
+//     postfetch('/colors',{color:[...arr]}).then((data)=>{
+//         return data.color
+//     })
+// })
 
