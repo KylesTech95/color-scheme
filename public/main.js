baf48a8778d69fdb8bae88b84b2a947115be2215
kylestech95 <kylestech95@gmail.com>
2024-04-13 19:19:19 -0400

    feat: add buttons - scroll

--- public/main.js
+++ public/main.js
@@ -1,21 +1,23 @@
 const spots = document.querySelectorAll('.choice-spot')
 let r,g,b,colour;
-// const postfetch = async(api,d) => {
-//     const response = await fetch(api,
-//     {
-//       method:'POST',
-//       mode:"cors",
-//       cache:"no-cache",
-//       credentials: "same-origin", // include, *same-origin, omit
-//       headers: { "Content-Type": "application/json"},
-//       redirect: "follow", // manual, *follow, error
-//       referrerPolicy: "no-referrer",
-//       body:JSON.stringify(d),
-//     })
-//     // testing postFetch
-//     return response.json()
-// }
-
+const insert_btn = document.getElementById('insert-color')
+const insert_color = '/colors-insert'
+const postfetch = async(api,d) => {
+    const response = await fetch(api,
+    {
+      method:'POST',
+      mode:"cors",
+      cache:"no-cache",
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: { "Content-Type": "application/json"},
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer",
+      body:JSON.stringify(d),
+    })
+    // testing postFetch
+    return response.json()
+}
+const api_colors = `/colors`
 
 let arr = []
 for(let i=0; i < 1<<12; i++) {
@@ -27,9 +29,42 @@ for(let i=0; i < 1<<12; i++) {
     arr.push(colour)
     
 }
-// spots[0].addEventListener('click', e => {
-//     postfetch('/colors',{color:[...arr]}).then((data)=>{
-//         return data.color
-//     })
-// })
 
+insert_btn.style.left = `${document.body.clientWidth/2}px`
+insert_btn.addEventListener('click', e => {
+    postfetch(insert_color,{color:[...arr]}).then((data)=>{
+        return data.color
+    })
+})
+
+
+
+window.onload=()=>{
+    fetch(api_colors).then(res=>res.json()).then(data=>{
+        spots.forEach(spot=>{
+            // console.log(data.colors)
+            let arr = [...data.colors]
+            for(let i = 0; i < arr.length; i++){
+                const li = document.createElement('li')
+                li.style = `background:${arr[i].color};
+                            position:absolute;
+                            height:100%;
+                            width:100%;
+                            display:flex;
+                            flex-direction:column;
+                            align-items:center;
+                            justify-content:center;
+                            `
+                    spot.appendChild(li)
+                    console.log(li)
+                // setTimeout(()=>{
+                //     spot.appendChild(li)
+                //     console.log(li)
+                // },25*(i+1))
+                
+            }
+            })
+        })
+}
+
+            
