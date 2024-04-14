1ada781c0ac4a4d1cb5a5a5759fa63518e7d446d
kylestech95 <kylestech95@gmail.com>
2024-04-14 13:21:43 -0400

    fork: color rotation

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
 const spot = document.querySelector('.choice-spot')
 let r,g,b,colour;
 const insert_btn = document.getElementById('insert-color')
-const insert_color = '/colors-insert'
+const scrolls = document.querySelectorAll('#scroll-container>.scroll')
+let idCount = 0;
+
+const failSafe = {
+    scroll: (scroll) => {
+        scroll.classList.add('no-pointer')
+        setTimeout(()=>{
+            scroll.classList.remove('no-pointer')
+        },2500)
+    }
+}
 const postfetch = async(api,d) => {
     const response = await fetch(api,
     {
@@ -17,86 +27,46 @@ const postfetch = async(api,d) => {
     // testing postFetch
     return response.json()
 }
-const rotateRight = (arr) => {
-    // arr[arr.length-1].classList.remove('swoosh-left')
-    let take = arr.pop();
-    arr.unshift(take)
-    // arr[arr.length-1].classList.add('swoosh-left')
-    arr[0].classList.add('z-index')
-    
-}
-const rotateLeft = (arr) => {
-    let take = arr.shift();
-    arr.push(take)
-    arr[0].classList.add('z-index')
-}
-const left_scroll = document.querySelectorAll('#scroll-container>.scroll')
-
-const api_colors = `/colors`
-
-let arr = []
-for(let i=0; i < 1<<12; i++) {
-    r = ((i>>8) & 0xf) * 0x11;
-    g = ((i>>4) & 0xf) * 0x11;
-    b = (i & 0xf) * 0x11;
-    colour = "rgb("+r+","+g+","+b+")";
-    // console.log(colour)
-    arr.push(colour)
-    
-}
-
-// insert_btn.style.left = `${document.body.clientWidth/2}px`
-// insert_btn.addEventListener('click', e => {
-//     postfetch(insert_color,{color:[...arr]}).then((data)=>{
-//         return data.color
-//     })
-// })
-
-
-
-window.onload=()=>{    
-    fetch(api_colors).then(res=>res.json()).then(data=>{
-            let arg = [...data.colors]
-            for(let i = 0; i < arg.length; i++){
-                const li = document.createElement('li')
-                const input = document.createElement('input')
-                      input.classList.add('color-input')
-                      input.setAttribute('type','text')
-                      input.setAttribute('value',arg[i].color);
-                      if(/[0-34],[0-34]|[0-34],/g.test(arg[i].color)){
-                        input.style.color = `ghostwhite`;
-                      }
-                      li.append(input)
-                li.style = `background:${arg[i].color};
-                            position:absolute;
-                            height:100%;
-                            width:100%;
-                            display:flex;
-                            flex-direction:column;
-                            align-items:center;
-                            justify-content:center;
-                            `
-                    spot.appendChild(li)
-                    
-
-                
-            }
-            let items = document.querySelectorAll('.choice-spot>li')
-            let arr = [...items]
-
-            left_scroll.forEach((sc,index)=>{
-                
-                    sc.addEventListener('click',e=>{
-                        arr.forEach(a=>a.classList.remove('z-index'))
-                        // console.log(e.target)
-                        if(index%2!==0)rotateRight(arr)
-                        if(index%2===0)rotateLeft(arr)
-
-                    })
-                
-                
-            })
+const rotateRight = (scroll) => {
+    scroll.addEventListener('click', e => {
+        failSafe.scroll(scroll);
+        idCount+=1;
+        if(idCount>4096)idCount=1;
+        console.log(idCount)
+        fetch(`/colors-insert/${idCount}`).then(res=>res.json()).then(data=>{
+            console.log(data.current_color)
         })
+    })
+}
+const rotateLeft = (scroll) => {
+    scroll.addEventListener('click', e => {
+        failSafe.scroll(scroll);
+        idCount-=1;
+        if(idCount<1)idCount=4096;
+        console.log(idCount)
+        
+        fetch(`/colors-insert/${idCount}`).then(res=>res.json()).then(data=>{
+            console.log(data.current_color)
+        })
+    })
 }
 
+
+const clickScrolls = () => {
+    scrolls.forEach((scroll,index)=>{
+        switch(true){
+            case index%2==0:
+                return rotateLeft(scroll);
+            break;
+            case index%2!==0:
+                return rotateRight(scroll);
+            break;
+            default:
+                console.log(undefined);
+            break;
+        }
+        
+    })
+}
+clickScrolls()
         
