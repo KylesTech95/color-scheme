e6ff1a28508e3d4efcb7853cdae315c297c72c9a
kylestech95 <kylestech95@gmail.com>
2024-04-16 07:42:24 -0400

    fork: bring back insert

--- public/main.js
+++ public/main.js
@@ -257,3 +257,22 @@ clickScrolls()
 
 // test sumFn
 //________________________________________
+
+let arr = []
+for(let i=0; i < 1<<12; i++) {
+    r = ((i>>8) & 0xf) * 0x11;
+    g = ((i>>4) & 0xf) * 0x11;
+    b = (i & 0xf) * 0x11;
+    colour = "rgb("+r+","+g+","+b+")";
+    // console.log(colour)
+    arr.push(colour)
+    
+}
+console.log(arr)
+
+insert_btn.style.left = `${document.body.clientWidth/2}px`
+insert_btn.addEventListener('click', e => {
+    postfetch('/colors-insert',{rgb:[...arr]}).then((data)=>{
+        return data.color
+    })
+})
