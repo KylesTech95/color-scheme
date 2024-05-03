b09ec5d3c0c924440797da7f0bedf98aedf04a58
kylestech95 <kylestech95@gmail.com>
2024-05-03 16:40:05 -0400

    match text with validator

--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
+const router = express.Router()
+
 const PORT = !process.env.PORT ? 3000 : process.env.PORT
 const routes = require('./../lib/routes.js')
 const middleware = require('./../lib/middleware.js')
 
 middleware(app,express,cors,bodyParser)
-routes(app,pool,sequelize)
+routes(app,pool,sequelize,router)
 
 app.listen(PORT,()=>{
     console.log('Buddy, you are listening on port ' + PORT)
