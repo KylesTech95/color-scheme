9d6221799820cc4ab81531ca5c830dfe091fd9f4
kylestech95 <kylestech95@gmail.com>
2024-05-05 11:24:38 -0400

    test: fetch (get)  multiple colors in the form of an array

--- public/lookup.js
+++ public/lookup.js
@@ -1,17 +1,22 @@
 // form vars
 let subBtn = document.getElementById('lookup-submit')
 const addBtn = document.querySelectorAll('.add')
+const input = document.getElementById('search')
 const input_container = document.querySelector('.input-div')
 const append_container = document.querySelector('.append-div')
 let val;
-let input = document.getElementById('search')
 let prefix = 'http://'
 let host = window.location.host;
 let api = '/colors/lookup/?search='
-let query;
-const constructURL = () => {
-
+let query = []
+function setCursorPosition(inputElem, position) {
+    if (inputElem.setSelectionRange) {
+      inputElem.focus();
+      inputElem.setSelectionRange(position, position);
+    }
 }
+setCursorPosition(input, 4 )
+
 // fetch lookup colors api
 // fetch('/colors/lookup')
 subBtn.onsubmit = e => {
@@ -19,14 +24,18 @@ subBtn.onsubmit = e => {
 }
 subBtn.onclick = e => {
     e.preventDefault();
-    query = input.value;
-    fetch(api+query).then(res=>res.json()).then(d=>console.log(d.colors))
+    const inputs = document.querySelectorAll('.search-input')
+    query =[...inputs].map(x=>x.value).join`&search=`
+    fetch(api+query).then(res=>res.json()).then(d=>console.log(d))
 
 }
 const addInput = e => {
     const newDiv = document.createElement('div')
     const n_input = document.createElement('input')
     const newAdd = document.createElement('span')
+    n_input.setAttribute('type','text')
+    n_input.setAttribute('value','rgb()')
+    n_input.classList.add('search-input')
     newDiv.classList.add('input-div')
     newAdd.classList.add('add')
     newAdd.classList.add('add-input')
@@ -34,6 +43,13 @@ const addInput = e => {
     newDiv.appendChild(n_input)
     newDiv.appendChild(newAdd)
     append_container.appendChild(newDiv)
+    newAdd.addEventListener('click',addInput)
+    let parent = e.target.parentElement
+    parent.removeChild(e.target)
+    parent.style = 'width:100%;'
+    setCursorPosition(n_input, 4 )
+    
+
 }
 addBtn.forEach(btn=>btn.addEventListener('click',addInput))
-
+console.log(addBtn)
