25a5adf4cc264f29db01336b91d5f0bc167149ae
kylestech95 <kylestech95@gmail.com>
2024-05-04 23:26:32 -0400

    feat: append additional input

--- public/lookup.js
+++ public/lookup.js
@@ -1,5 +1,8 @@
 // form vars
 let subBtn = document.getElementById('lookup-submit')
+const addBtn = document.querySelectorAll('.add')
+const input_container = document.querySelector('.input-div')
+const append_container = document.querySelector('.append-div')
 let val;
 let input = document.getElementById('search')
 let prefix = 'http://'
@@ -20,3 +23,17 @@ subBtn.onclick = e => {
     fetch(api+query).then(res=>res.json()).then(d=>console.log(d.colors))
 
 }
+const addInput = e => {
+    const newDiv = document.createElement('div')
+    const n_input = document.createElement('input')
+    const newAdd = document.createElement('span')
+    newDiv.classList.add('input-div')
+    newAdd.classList.add('add')
+    newAdd.classList.add('add-input')
+    newAdd.textContent='+'
+    newDiv.appendChild(n_input)
+    newDiv.appendChild(newAdd)
+    append_container.appendChild(newDiv)
+}
+addBtn.forEach(btn=>btn.addEventListener('click',addInput))
+
