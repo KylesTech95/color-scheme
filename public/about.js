fc2ff6c1458f7d84a27fbbdb8d0f19283d45c335
kylestech95 <kylestech95@gmail.com>
2024-05-10 11:18:22 -0400

    feat: update layout

--- public/about.js
+++ public/about.js
@@ -1,6 +1,7 @@
 const h2s = document.querySelectorAll('.h2-class')
 const video = {
-    keyboard: document.querySelector('.keyboard-video')
+    keyboard: document.querySelector('.keyboard-video'),
+    all:document.querySelectorAll('video')
 }
 h2s.forEach((h2,iterate)=>{
     setTimeout(()=>{
@@ -20,22 +21,33 @@ let URL = window.URL
 // let text = new Blob([data],{type: 'text/plain'})
 // let value = URL.createObjectURL(text)
 
-//http://127.0.0.1:5500/media/keyboard-vid.mp4
-let url = window.location.origin+'/media/keyboard-vid.mp4';
-fetch(url).then(res=>res.blob()).then(blob=>{
-    readFile(blob)
-    const file = new File([blob],'image',{type:blob.type})
-    console.log(file)
-})
 
-function readFile(input){
-    const fr = new FileReader();
-    fr.readAsDataURL(input)
+// blob with a video file 
+let url = './media/keyboard-vid.mp4';
+const xml = new XMLHttpRequest()
+const method = 'GET'
 
-    fr.addEventListener('load',e=>{
-        const res = fr.result;
-        console.log(res)
-    })
-}
+xml.open(method,url)
+xml.responseType = 'arraybuffer';
+
+// xml.onload = () => {
+//     let blob = new Blob([xml.response])
+//     let url = URL.createObjectURL(blob)
+//     video.keyboard.src=url
+//     video.keyboard.play()
+    
+// }
+    xml.onload = () => {
+        video.all.forEach(vid=>{
+            console.log(vid)
+            let blob = new Blob([xml.response])
+            let url = URL.createObjectURL(blob)
+            console.log(url)
+            vid.src=url
+            vid.play()
+        })
+    }
+
+xml.send()
 
 
