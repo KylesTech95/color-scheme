f12eb46100028a0c1699e6570e4003b82a66573c
kylestech95 <kylestech95@gmail.com>
2024-05-10 16:45:08 -0400

    fork: about page

--- public/about.js
+++ public/about.js
@@ -9,45 +9,6 @@ h2s.forEach((h2,iterate)=>{
         h2.classList.remove('translate-restore')
     },250*(iterate+1))
 })
-// const video_location = window.location.origin+'/../media/keyboard-vid.mp4'
-// load video statically
-// video.keyboard.src = video_location
-// console.log(video.keyboard)
 
-// load text as a blob
-let URL = window.URL
-// let data = 'testing data here'
-
-// let text = new Blob([data],{type: 'text/plain'})
-// let value = URL.createObjectURL(text)
-
-
-// blob with a video file 
-let url = './media/keyboard-vid.mp4';
-const xml = new XMLHttpRequest()
-const method = 'GET'
-
-xml.open(method,url)
-xml.responseType = 'arraybuffer';
-
-// xml.onload = () => {
-//     let blob = new Blob([xml.response])
-//     let url = URL.createObjectURL(blob)
-//     video.keyboard.src=url
-//     video.keyboard.play()
-    
-// }
-    xml.onload = () => {
-        video.all.forEach(vid=>{
-            console.log(vid)
-            let blob = new Blob([xml.response])
-            let url = URL.createObjectURL(blob)
-            console.log(url)
-            vid.src=url
-            vid.play()
-        })
-    }
-
-xml.send()
 
 
