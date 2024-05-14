55220d8b97e6810df648f6ef03b481c3419cf144
kylestech95 <kylestech95@gmail.com>
2024-05-14 08:03:56 -0400

    fork: features placement mobile

--- public/about.js
+++ public/about.js
@@ -64,7 +64,7 @@ media_features.forEach((feat,index)=>{
         if(/mp4/.test(ext)&&e.target.muted){
             e.target.play();
             e.target.classList.remove('video-paused');
-            e.target.style=`z-index:999;opacity:1;${'transform:scale(.8)'};border:2rem solid #333;`
+            e.target.style=`z-index:999;opacity:1;${'transform:scale(1.5)'};`
             // console.log(labels[index])
         }
     }
